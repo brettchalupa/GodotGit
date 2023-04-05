3179edfba6c8166407c2dba31be5a9f7ae2507da
Brett Chalupa <brettchalupa@gmail.com>
2023-04-05 09:29:48 -0400

    Toggle Godot robot icon with spacebar
    To help with motion sickness

--- main.gd
+++ main.gd
@@ -1,4 +1,10 @@
 extends Node2D
 
+var rotating = true
 func _process(delta: float) -> void:
-	$Icon.rotate(delta)
+	if rotating:
+		$Icon.rotate(delta)
+
+func _input(event: InputEvent) -> void:
+	if event.is_action_pressed("ui_accept"):
+		rotating = !rotating
